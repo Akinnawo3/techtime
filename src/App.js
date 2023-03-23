618e4125940a8d973aff9d732a2cd7afbf69b4fe
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-23 20:49:30 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import logo from "./logo.svg";
 import {BrowserRouter as Router} from "react-router-dom";
 import {Routes, Route} from "react-router";
 import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import Courses from "./pages/Courses";
+import Community from "./pages/Community";
+import Enroll from "./pages/Enroll";
+import Testimonial from "./pages/Testimonial";
 
 
 function App() {
   const routes = [
-    { path: "/", component: ()=><h1>Home</h1> },
+    { path: "/", component: Home },
+    { path: "/about", component: AboutUs },
+    { path: "/courses", component: Courses },
+    { path: "/testimonial", component: Testimonial },
+    { path: "/community", component: Community },
+    { path: "/enroll", component: Enroll },
   ];
 
   return (
