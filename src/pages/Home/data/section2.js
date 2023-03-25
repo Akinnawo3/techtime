465c931ab780859cd7c094fe65bb3ba494b09a5b
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-25 09:06:57 +0100

    affordable prices

--- src/pages/Home/data/section2.js
+++ src/pages/Home/data/section2.js
@@ -1,3 +1,22 @@
 export const itemsList = [
-    
-]
+  {
+    icon: "/images/sec2-ic1.png",
+    title: "Experienced Mentors",
+    body: "High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. ",
+  },
+  {
+    icon: "/images/sec2-ic2.png",
+    title: "One-On-One Meetings",
+    body: "High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. ",
+  },
+  {
+    icon: "/images/sec2-ic3.png",
+    title: "One-On-One Meetings",
+    body: "High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. ",
+  },
+  {
+    icon: "/images/sec2-ic4.png",
+    title: "Affordable Prices",
+    body: "High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. ",
+  },
+];
