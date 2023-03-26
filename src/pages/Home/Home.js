57febc54f44db488fba50a8271af217eca74c258
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-26 18:08:11 +0100

    tope

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,6 +7,7 @@ import Section2 from "./components/Section2";
 import Section3 from "./components/Section3";
 import Section4 from "./components/Section4";
 import Section5 from "./components/Section5";
+import Section6 from "./components/Section6";
 
 const Home = () => {
   return (
@@ -18,6 +19,7 @@ const Home = () => {
       <Section3 />
       <Section4 />
       <Section5 />
+      <Section6 />
     </div>
   );
 };
---
+++ src/pages/Home/components/Section6.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Section6 = () => {
+  return (
+    <div
+      className="side-padding py-4 section-5 text-white"
+      style={{ backgroundColor: "#004DB3" }}
+    >
+      <div
+        className="fw-bold clash-d-f text-center bg-left-text position-relative mt-5"
+        style={{ fontSize: 43, lineHeight: "55px" }}
+      >
+        What Our Clients Are Saying
+      </div>
+      <div className="w-text mx-auto px-5 ">
+        <small className="sm-left-text" style={{ color: "#A1A1A1" }}>
+          High-Defination Video Is Video Of Higher Resolution And Quality Than
+          Standard Definition. While There’s No Standard Meaning For High
+          Definition, Generally Sny Standard Video Image
+        </small>
+      </div>
+    </div>
+  );
+};
+
+export default Section6;
