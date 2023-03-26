3f3d6ba9be69e2840ef8607264724fa3c24bd1e7
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-26 18:13:51 +0100

    accordion design

--- src/pages/Home/components/Section6.js
+++ src/pages/Home/components/Section6.js
@@ -1,16 +1,26 @@
 import React from "react";
 
 const Section6 = () => {
+  const questions = [
+    "Is there a free trial available?",
+    "Can i change my plan later?",
+    "Are the courses lifetime?",
+    "Do I get certified after taking courses?",
+    "How do I reach out to mentors?",
+    "Do we get job ready after taking courses?"
+
+
+  ];
   return (
     <div
-      className="side-padding py-4 section-5 text-white"
-      style={{ backgroundColor: "#004DB3" }}
+      className="side-padding py-4 section-5"
+      //   style={{ backgroundColor: "#004DB3" }}
     >
       <div
         className="fw-bold clash-d-f text-center bg-left-text position-relative mt-5"
         style={{ fontSize: 43, lineHeight: "55px" }}
       >
-        What Our Clients Are Saying
+        Frequently Asked Questions
       </div>
       <div className="w-text mx-auto px-5 ">
         <small className="sm-left-text" style={{ color: "#A1A1A1" }}>
